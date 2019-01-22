00fcf1efd15e84e00c98c26ae37a364e2e8c57da
Vincent Tam <vtam2012@gmail.com>
2019-01-21 20:24:16 -0800

    initialize repo

---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const {User} = require('../models/user');
+const validate = require('../middleware/validate');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', validate(validateAuth) ,async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Invalid email or password.');
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Invalid email or password.');
+
+  const token = user.generateAuthToken();
+  res.send(token);
+});
+
+function validateAuth(req) {
+  const schema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required()
+  };
+
+  return Joi.validate(req, schema);
+}
+
+module.exports = router; 
